d4c3a3b9c7e2bf2f03e7eb5cb9a05004feb5950c
Dexter Pratt <dexterpratt.bio@gmail.com>
2014-11-11 10:47:41 -0800

    Minor updates to test.py

--- test.py
+++ test.py
@@ -7,8 +7,15 @@ Created on Mon Oct  6 22:34:42 2014
 import ndex_client as nc
 import ndex_util as util
 
-myNdex = nc.Ndex("http://dev.ndexbio.org")
-#myNet = myNdex.getNetworkByEdges("63177354-433b-11e4-9369-90b11c72aefa", 0 , 25)
+# Establish an object to communicate with the NDEx Test Server 
+# Anonymous connection, can perform only Read operations
+myNdex = nc.Ndex("http://test.ndexbio.org")
+# myNet = myNdex.getNetworkByEdges("63177354-433b-11e4-9369-90b11c72aefa", 0 , 25)
+# 1ada3330-45cc-11e4-a9e5-000c29873918
+#myNet = myNdex.get_neighborhood('1ada3330-45cc-11e4-a9e5-000c29873918', 'BRAF RAF1 KRAS NRAS HRAS MAPK1 MAPK2 MAP2K1 MAP2K2')
+# BEL Large Corpus on NDEx TEST server
 myNet = myNdex.get_neighborhood('1ada3330-45cc-11e4-a9e5-000c29873918', 'BRAF RAF1 KRAS NRAS HRAS MAPK1 MAPK2 MAP2K1 MAP2K2')
+
+# The utility wrapper is a work in progress to provide useful manipulations of networks
 myWrapper = util.NetworkWrapper(myNet)
 myWrapper.write_summary("RAS-RAF-MEK-MAPK.txt")
